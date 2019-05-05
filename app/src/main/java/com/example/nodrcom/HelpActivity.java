3e1fa01e206bac1f2a1b13a3dff3a0f4401aa69e
wickyan <yanweiqi199888@126.com>
2019-05-05 23:37:21 +0800

    v1.0.3

---
+++ app/src/main/java/com/example/nodrcom/HelpActivity.java
@@ -0,0 +1,23 @@
+package com.example.nodrcom;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        new myClass().fullScreen(this);//设置透明状态栏
+
+    }
+
+}
